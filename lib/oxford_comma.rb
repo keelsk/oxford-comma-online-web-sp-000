65a594f184fe148a816d8fa61d7f329895918234
Kirsten Keels <keelsk@gmail.com>
2020-02-23 16:59:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if (array.length == 1)
     array
   elsif (array.length == 2)
-    array.join("and")
+    array.join(" and ")
   else
     array[-1] = "and " + array[-1]
     array.join(",")
